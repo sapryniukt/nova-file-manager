6193d4108c81de196864694b28346de6aa7d70b0
Taras Sapryniuk <sapryniukt@gmail.com>
2026-01-05 11:39:09 +0100

    fix: update ScrollArea height to max-h-80 for better responsiveness in DetailField.vue

--- resources/js/fields/DetailField.vue
+++ resources/js/fields/DetailField.vue
@@ -6,7 +6,7 @@
       hidden: field.value.length === 0,
     }"
   >
-    <ScrollArea class="h-80 pr-4">
+    <ScrollArea class="max-h-80 pr-4">
       <h3 v-if="field.showCard" class="text-xl font-bold">{{ field.name }}</h3>
       <PanelItem :field="field" :index="index" class="flex-1 !border-t-0 px-0">
         <template v-if="field.value" v-slot:value>
